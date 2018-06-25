3458b6e896839a3d43cc76bbcd9a7b8d7a63b565
Kabin Dulal <dulalkabin99@gmail.com>
2018-06-25 14:09:30 -0400

    first commit

---
+++ src/main/java/com/dulal/lesson06forms1/Lesson06forms1Application.java
@@ -0,0 +1,12 @@
+package com.dulal.lesson06forms1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson06forms1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson06forms1Application.class, args);
+	}
+}
---
+++ src/test/java/com/dulal/lesson06forms1/Lesson06forms1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dulal.lesson06forms1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lesson06forms1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
